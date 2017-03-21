fc9f48f42d437fd1cf91305080f34393b9b9719e
Gaby <gaby.ranarz@gmail.com>
2017-03-21 16:38:56 -0600

    traduccion de formulario y muestra los datos ingresados

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function translate(){
+  var title = document.getElementById("form-signin-heading");
+  var email = document.getElementById("inputEmail");
+  var password = document.getElementById("inputPassword");
+  var remember = document.getElementsByTagName("span")[0];
+  var button= document.getElementsByClassName("btn")[0];
+
+  title.innerHTML = "Por favor inicia sesión";
+  email.placeholder = "Correo Electrónico";
+  password.placeholder = "Contraseña";
+  remember.innerHTML = "Recordar datos";
+  button.innerHTML = "Iniciar Sesión";
+
+}
+
+translate();
+
+function mostrarDatos(){
+  var email = document.getElementById("inputEmail").value;
+  var password = document.getElementById("inputPassword").value;
+  document.getElementById("mostrarCorreo").innerHTML= email;
+  document.getElementById("mostrarPass").innerHTML= password;
+}
